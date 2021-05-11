77e02eb610811be3b39a388ca661a9b24d753b1b
Sonali Singh <sonali122000@gmail.com>
2021-05-11 23:58:32 +0530

    add card components

--- web/src/components/Layout.js
+++ web/src/components/Layout.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import {Row, Col, Card} from "react-bootstrap";
+import { Row, Col, Card } from "react-bootstrap";
 import Map from "./Map"
 // import Header from "./Header"
 
 const Layout = () => {
-return (
-    <div className="page-body">
-    <Row>
-    <Col lg={8}>
-    <Card>
-    <Map />
-    </Card>
-    </Col>
-    </Row>
-    </div>
-)
+    return (
+        <div className="page-body">
+            <Row>
+                <Col lg={8}>
+                    <Card>
+                        <Card.Body>
+                            <Map />
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                <Col lg={4}>
+                    <Card>
+                        <Card.Body>
+                            Route
+           </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    )
 }
 
 export default Layout;
