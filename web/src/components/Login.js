7e26e243d49c78dff3c992eef781df3894a193cd
naveen8801 <naveensharma10d@gmail.com>
2021-05-14 00:19:11 +0530

    Login page

---
+++ web/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../assets/scss/components/login.scss'
+
+function Login() {
+    return (
+        <div    >
+            
+        </div>
+    )
+}
+
+export default Login
